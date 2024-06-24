808c11baddc61f668fe2728cb32117f4484956d1
root <root@tak-docker-vm.4cxakttdyusuhpob1hixueafgd.syx.internal.cloudapp.net>
2024-06-24 05:08:08 +0000

    2tier-flask-app

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DATABASE')}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
